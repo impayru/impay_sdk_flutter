0bccf4c903d44be0567ff0a05dab0e3f706a7507
anton <a.yriev@teleport.run>
2021-05-07 12:56:15 +0300

    upd 3dsform

--- lib/pay3ds.dart
+++ lib/pay3ds.dart
@@ -29,25 +29,22 @@ class _PayWebViewState extends State<PayWebView> {
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
-  JavascriptChannel _channel = JavascriptChannel(
-    name: 'jsMessageReceived',
-    onMessageReceived: (JavascriptMessage message) {
-      print(message);
-      if (message.message == 'impayapp::close()') {
-        //Navigator.pop(context);
-      }
-  });
+  void pageStartedCallback(String url) {
+    print(url);
+    if (url == 'impayapp::close()') {
+      Navigator.pop(context);
+    }
+  }
 
   String _3dsData(url, pa, md, creq, termurl) {
     return Uri.dataFromString(
-        '<body onload="onload()">тест'+
+        '<body onload="onload()">'+
             '<form name="formpay" action="'+url+'" method="POST">'+
               (creq.isNotEmpty ?
                 '<input type="hidden" name="creq" value="'+creq+'">' :
                 ('<input type="hidden" name="PaReq" value="'+pa+'">'+ '<input type="hidden" name="MD" value="'+md+'">')
               ) +
               '<input type="hidden" name="TermUrl" value="'+termurl+'">'+
-              '<input type="submit" value="submit">'+
             '</form>'+
             '<script>document.formpay.submit();</script>'+
         '</body>',
@@ -59,7 +56,7 @@ class _PayWebViewState extends State<PayWebView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: <Widget>[
+    return Scaffold(body: ListView(children: <Widget>[Column(children: <Widget>[
       Container(
         alignment: Alignment.bottomRight,
         padding: EdgeInsets.only(top: 30.0),
@@ -68,17 +65,17 @@ class _PayWebViewState extends State<PayWebView> {
         })
       ),
       Container(
-        height: MediaQuery.of(context).size.height - 30.0,
+        height: MediaQuery.of(context).size.height - 100.0,
         child: WebView(
           initialUrl: _3dsData(widget.url, widget.pa, widget.md, widget.creq, widget.termurl),
           javascriptMode: JavascriptMode.unrestricted,
-          javascriptChannels: {_channel},
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller = webViewController;
-              _controller!.loadUrl(_3dsData(widget.url, widget.pa, widget.md, widget.creq, widget.termurl));
-            }
+          onPageStarted: pageStartedCallback,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller = webViewController;
+            _controller!.loadUrl(_3dsData(widget.url, widget.pa, widget.md, widget.creq, widget.termurl));
+          }
         )
       )
-    ]));
+    ])]));
   }
 }
